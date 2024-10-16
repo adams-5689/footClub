1faad1e27f92545c1c9b34577790698c05dc1ce1
adama <adamadiabyblacky@gmail.com>
2024-10-16 15:44:44 +0000

    finalisation de la sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 
-import Sidebar from './components/sidebar';
+import Sidebar, { SidebarItem } from './components/sidebar';
 import './App.css';
-
+import {
+  LifeBuoy,
+  Receipt,
+  Boxes,
+  Package,
+  UserCircle,
+  BarChart3,
+  LayoutDashboard,
+  Settings
+} from "lucide-react"
+import React from 'react';
 function App() {
 
   return (
     <>
-    <Sidebar></Sidebar>
+    <Sidebar>
+      <SidebarItem icon={<BarChart3 size={20} />} text="Dashboard" alert/>
+      <SidebarItem icon={<UserCircle size={20} />} text="User" />
+      <SidebarItem icon={<Boxes size={20} />} text="Inventory" />
+      <SidebarItem icon={<Package size={20} />} text="Orders" alert />
+      <SidebarItem icon={<Receipt size={20} />} text="Billings" alert />
+      <hr className='my-3'/>
+      <SidebarItem icon={<Settings size={20} />} text="Settings" alert />
+      <SidebarItem icon={<LifeBuoy size={20} />} text="Help" alert />
+
+    </Sidebar>
     </>
   )
 }
